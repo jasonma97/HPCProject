3b134b86a2d5dd80a1f171daf174cdc4b5ec16d5
jasonma97 <ma.jason97@gmail.com>
2016-12-03 19:27:51 -0800

    Adding tester testing suite

---
+++ openmppythontest.py
@@ -0,0 +1,39 @@
+from multiprocessing import Pool
+from time import time
+def sum_nums(args):
+    low = int(args[0])
+    high = int(args[1])
+    return sum(range(low,high+1))
+
+def testCode(n, procs):
+    results = []
+    sizeSegment = n/procs
+
+    if procs == 1:
+        start = time()
+        print(sum_nums([0, n]))
+        t = time() - start
+        print("Serial Runtime:" + str(t))
+        return [procs, t]
+
+
+    # Multiprocessing version 
+    # Create size segments list
+    start2 = time()
+    jobs = []
+    for i in range(0, procs):
+        jobs.append((i*sizeSegment+1, (i+1)*sizeSegment))
+
+    pool = Pool(procs).map(sum_nums, jobs)
+    result = sum(pool)
+    print(result)
+    t = time() - start2
+    print("Parallel Runtime ({0} Workers):".format(procs) + str(time() - start2))
+    return [procs, t]
+
+if __name__ == "__main__":
+    results = []
+    for i in range(1, 6):
+        results.append(testCode(400000, i))
+    print(results)
+
