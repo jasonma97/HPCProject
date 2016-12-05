1a3568cbd42dec9583b1d88d1534708e36988f7d
jasonma97 <ma.jason97@gmail.com>
2016-12-04 22:21:36 -0800

    WE GOT SOME DATA

--- RabinCDC.py
+++ RabinCDC.py
@@ -22,6 +22,10 @@ for ; i < n; i++; do
 return i;
 '''
 import sys
+from multiprocessing import Pool
+from time import time
+numProcs  = 4
+
 
 def string2numeric_hash(text):
     import hashlib
@@ -35,17 +39,19 @@ def RabinCDC(LBAlist):
     breakindices = []
     for i in range(a,n-1):
         rolling_hash += sum(LBAlist[i-a:i])
-        secondhash = string2numeric_hash(str(rolling_hash))
+        secondhash = string2numeric_hash(str(rolling_hash).encode('utf-8'))
         if secondhash%9 == 0:
             breakindices.append(i)
             rolling_hash = 0
     return breakindices
 
+
+
 def main(src):
     '''
     Input: data buffer src
     '''
-    target = open(src, 'r')
+    target = open(src, 'r', encoding='utf-8')
 
     LBAlist = target.readlines()
     LBAlist = [int(x) for x in LBAlist]
@@ -58,9 +64,65 @@ def main(src):
     outputfile.close()
 
 
+
+def serialCode(LBAlist):
+    '''
+    Input: data buffer src
+    '''
+
+    breakindices = RabinCDC(LBAlist)
+    #print(breakindices)
+    #print(len(breakindices))
+    outputfile = open("RabinOut", 'w')
+    for i in breakindices:
+        outputfile.write("%s\n" % i)
+    outputfile.close()
+
+
+def timeTrials(src):
+    target = open(src, 'r')
+    global LBAlist
+    LBAlist = target.readlines()
+    LBAlist = [int(x) for x in LBAlist]
+
+
+
+    start = time()
+    serialCode(LBAlist)
+    t = time() - start
+    print(t)
+    time_list = []
+    global numProcs
+
+    #MultiProcessor Run
+    for i in range(2, numProcs + 1):
+        start = time()
+        chunkSize = int(len(LBAlist)/numProcs)
+        jobs = []
+        for p in range(0,i):
+            if p == 0:
+                jobs.append((LBAlist[:chunkSize]))
+            elif p != i-1 and p > 0:
+                jobs.append((LBAlist[chunkSize*p:chunkSize*(p+1)]))
+            else:
+                jobs.append((LBAlist[chunkSize*p:]))
+        pool = Pool(i).map(RabinCDC, jobs)
+        newPool = []
+        for i in pool:
+            newPool += i
+        #print(newPool)
+        #print(len(newPool))
+
+        t2 = time() - start
+        print(t2)
+        time_list.append([i, t2])
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         src = sys.argv[1] # data buffer
-        main(src)
+        timeTrials(src)
+    elif len(sys.argv) == 1:
+        timeTrials('homes1')
     else:
-        print "Usage: fastcdc.py <databuffer>"
+        print("Usage: fastcdc.py <databuffer>")
